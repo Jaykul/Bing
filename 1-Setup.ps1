258bee7812b8f7fb4a78f211e786cc861fb6db40
Joel Bennett <Jaykul@HuddledMasses.org>
2015-12-03 11:45:26 -0500

    Fix setup script

--- 1-Setup.ps1
+++ 1-Setup.ps1
@@ -12,11 +12,13 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 Write-Host "SETUP $ModuleName in $Path"
 
-$PackagePaths = Get-ChildItem $Path -Recurse -Filter packages.config
+
+$NuGetApi = 'https://www.nuget.org/api/v2'
+$PackagePaths = Get-ChildItem $Path, (Join-Path $Path *\*) -Filter packages.config
 if($PackagePaths) {
     if(!($Name = Get-PackageSource | ? Location -match 'nuget.org/api/v2' | % Name)) {
         Write-Warning "Adding NuGet package source"
-        $Name = Register-PackageSource NuGet -Location 'https://www.nuget.org/api/v2' -ForceBootstrap -ProviderName NuGet | % Name
+        $Name = Register-PackageSource NuGet -Location $NuGetApi -ForceBootstrap -ProviderName NuGet | % Name
     }
 
     # recursively handle packages.config
@@ -28,6 +30,7 @@ if($PackagePaths) {
         # This recreates nuget's package restore, but hypothetically, with support for any provider
         # E.g.: nuget restore -PackagesDirectory "$Path\packages" -PackageSaveMode nuspec
         foreach($Package in ([xml](Get-Content $config.FullName)).packages.package) {
+            if(!$Package.Source) { $Package.Source = $NuGetApi }
             Write-Verbose "Installing $($Package.id) v$($Package.version) from $($Package.Source)"
             $install = Install-Package -Name $Package.id -RequiredVersion $Package.version -Source $Package.Source -Destination $Path\packages -PackageSave nuspec -Force -ErrorVariable failure
             if($failure) {
@@ -36,5 +39,6 @@ if($PackagePaths) {
         }
     }
 }
-
+Push-Location $Path
 git submodule update --init --recursive
+Pop-Location
